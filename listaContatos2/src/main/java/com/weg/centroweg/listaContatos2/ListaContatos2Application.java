070b182d95b330b018989faa3d48f1f1ac73785f
Kaua Felix <kfelixcosta11@gmail.com>
2026-02-17 20:53:40 -0300

    feat: inicializa aplicação Spring Boot para gerenciamento de contatos

---
+++ listaContatos2/src/main/java/com/weg/centroweg/listaContatos2/ListaContatos2Application.java
@@ -0,0 +1,13 @@
+package com.weg.centroweg.listaContatos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaContatos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaContatos2Application.class, args);
+	}
+
+}
---
+++ listaContatos2/src/test/java/com/weg/centroweg/listaContatos2/ListaContatos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.centroweg.listaContatos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaContatos2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
